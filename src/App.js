ccc00f2db157b841f3f8b7ee08e670afd5891848
Kashyap Patel <kash@Kashyaps-MacBook-Pro.local>
2024-02-15 14:57:54 -0500

    final

---
+++ src/App.js
@@ -0,0 +1,70 @@
+// import React from "react";
+// import "./App.css";
+
+// import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+// import Menu from "./Menu/Menu";
+// import Hero from "./Hero/Hero";
+// import HomePage from "./HomePage/HomePage";
+// import Footer from "./Footer/Footer";
+// import AboutPage from "./AboutPage/AboutPage";
+// import LoginPage from "./LoginPage/LoginPage";
+
+// function App() {
+//   return (
+//     <Router>
+//       <Menu />
+//       <Hero />
+//       <div className="mainContainer">
+//         <Switch>
+//           <Route path="/about">
+//             <AboutPage />
+//           </Route>
+//           <Route path="/login">
+//             <LoginPage />
+//           </Route>
+//           <Route path="/">
+//             <HomePage />
+//           </Route>
+//         </Switch>
+//       </div>
+//       <Footer />
+//     </Router>
+//   );
+// }
+
+// export default App;
+
+
+
+
+import React from "react";
+import "./App.css";
+
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import Menu from "./Menu/Menu";
+import Hero from "./Hero/Hero";
+import HomePage from "./HomePage/HomePage";
+import Footer from "./Footer/Footer";
+import AboutPage from "./AboutPage/AboutPage";
+import LoginPage from "./LoginPage/LoginPage";
+
+function App() {
+  return (
+    <Router>
+      <Menu />
+      <Hero />
+      <div className="mainContainer">
+        <Routes>
+          <Route path='/' element={<HomePage/>} />
+          <Route path='/about' element={<AboutPage/>} />
+          <Route path='/login' element={<LoginPage/>} />
+        </Routes>
+      </div>
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;
